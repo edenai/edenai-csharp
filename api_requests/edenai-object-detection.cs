5fb1dafcd91fa6c690bb89d3f853f109a6154878
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:43:13 +0200

    Update edenai-object-detection.cs

--- api_requests/edenai-object-detection.cs
+++ api_requests/edenai-object-detection.cs
@@ -2,8 +2,8 @@ var client = new RestClient("https://api.edenai.run/v1/pretrained/vision/object_
 client.Timeout = -1;
 var request = new RestRequest(Method.POST);
 request.AddHeader("Authorization", "Bearer your_api_key");
-request.AddParameter("providers", "[\'google_cloud\', \'microsoft\', \'aws\', \'ibm\']");
-request.AddFile("files", "/Downloads/example.mp3");
+request.AddParameter("providers", "[\'google\', \'microsoft\', \'aws\', \'ibm\']");
+request.AddFile("files", "/Downloads/example.png");
 request.AddParameter("objects_to_find", "'squirrel'");
 IRestResponse response = client.Execute(request);
 Console.WriteLine(response.Content);
