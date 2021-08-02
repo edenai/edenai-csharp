c26c497024592a5f2c8e6487f8ffeec73bba3ba8
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:33:31 +0200

    Create edenai-sentiment-analysis.cs

---
+++ api_requests/edenai-sentiment-analysis.cs
@@ -0,0 +1,11 @@
+var client = new RestClient("https://api.edenai.run/v1/pretrained/text/sentiment_analysis");
+client.Timeout = -1;
+var request = new RestRequest(Method.POST);
+request.AddHeader("Authorization", "Bearer your_api_key");
+request.AlwaysMultipartFormData = true;
+request.AddParameter("providers", "[\'ibm\', \'microsoft\', \'aws\', \'google_cloud\']");
+request.AddParameter("text", "\'I am angry today\'");
+request.AddParameter("language", "en-US");
+request.AddParameter("sentiments_to_find", "neutral");
+IRestResponse response = client.Execute(request);
+Console.WriteLine(response.Content);
