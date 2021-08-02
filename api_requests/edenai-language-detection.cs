42c6d1fa8d51b93a94056a06622823c500dfd7dc
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:31:32 +0200

    Create edenai-language-detection.cs

---
+++ api_requests/edenai-language-detection.cs
@@ -0,0 +1,10 @@
+var client = new RestClient("https://api.edenai.run/v1/pretrained/text/language_detection");
+client.Timeout = -1;
+var request = new RestRequest(Method.POST);
+request.AddHeader("Authorization", "Bearer your_api_key");
+request.AlwaysMultipartFormData = true;
+request.AddParameter("providers", "[\'ibm\', \'microsoft\', \'aws\', \'google_cloud\']");
+request.AddParameter("text", "\'I am angry today\'");
+request.AddParameter("language", "en-US");
+IRestResponse response = client.Execute(request);
+Console.WriteLine(response.Content);
