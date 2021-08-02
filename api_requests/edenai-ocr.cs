571f6800752ff289ae6bd9c1c780e75db239fade
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:22:33 +0200

    Create edenai-ocr.cs

---
+++ api_requests/edenai-ocr.cs
@@ -0,0 +1,10 @@
+var client = new RestClient("https://api.edenai.run/v1/pretrained/vision/ocr");
+client.Timeout = -1;
+var request = new RestRequest(Method.POST);
+request.AddHeader("Authorization", "Bearer your_api_key");
+request.AddParameter("providers", "[\'google_cloud\', \'microsoft\', \'aws\', \'ocr_space\']");
+request.AddFile("files", "/Downloads/example.mp3");
+request.AddParameter("language", "en-US");
+request.AddParameter("text_reference", "'Hello world'");
+IRestResponse response = client.Execute(request);
+Console.WriteLine(response.Content);
