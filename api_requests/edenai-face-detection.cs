589679254cfa79f8de90e0f80abe36606354bc6f
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:42:49 +0200

    Update edenai-face-detection.cs

--- api_requests/edenai-face-detection.cs
+++ api_requests/edenai-face-detection.cs
@@ -3,6 +3,6 @@ client.Timeout = -1;
 var request = new RestRequest(Method.POST);
 request.AddHeader("Authorization", "Bearer your_api_key");
 request.AddParameter("providers", "[\'google_cloud\', \'microsoft\', \'aws\']");
-request.AddFile("files", "/Downloads/example.mp3");
+request.AddFile("files", "/Downloads/example.png");
 IRestResponse response = client.Execute(request);
 Console.WriteLine(response.Content);
