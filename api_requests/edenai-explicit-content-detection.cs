61ae5e19071d032847b3a2e71746898cd1bd5479
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:43:44 +0200

    Update edenai-explicit-content-detection.cs

--- api_requests/edenai-explicit-content-detection.cs
+++ api_requests/edenai-explicit-content-detection.cs
@@ -2,7 +2,7 @@ var client = new RestClient("https://api.edenai.run/v1/pretrained/vision/explici
 client.Timeout = -1;
 var request = new RestRequest(Method.POST);
 request.AddHeader("Authorization", "Bearer your_api_key");
-request.AddParameter("providers", "[\'google_cloud\', \'microsoft\', \'aws\', \'ibm\']");
+request.AddParameter("providers", "[\'google\', \'microsoft\', \'aws\', \'ibm\']");
 request.AddFile("files", "/Downloads/example.png");
 IRestResponse response = client.Execute(request);
 Console.WriteLine(response.Content);
