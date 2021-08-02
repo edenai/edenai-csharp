90dc19176caa39b1eeefeaf7452afa1ee2cc888c
Eden AI <67952486+edenai@users.noreply.github.com>
2021-08-02 15:35:22 +0200

    Create edenai-keyword-extraction.cs

---
+++ api_requests/edenai-keyword-extraction.cs
@@ -0,0 +1,11 @@
+var client = new RestClient("https://api.edenai.run/v1/pretrained/text/keyword_extraction");
+client.Timeout = -1;
+var request = new RestRequest(Method.POST);
+request.AddHeader("Authorization", "Bearer your_api_key");
+request.AlwaysMultipartFormData = true;
+request.AddParameter("providers", "[\'lettria\', \'microsoft\', \'aws\', \'ibm\']");
+request.AddParameter("text", "\'I am angry today\'");
+request.AddParameter("language", "en-US");
+request.AddParameter("keywords_to_find", "angry");
+IRestResponse response = client.Execute(request);
+Console.WriteLine(response.Content);
